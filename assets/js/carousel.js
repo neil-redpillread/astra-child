f95f0602dd3a623890c83f30a285dfe665305923
neil-redpillread <redpillread@gmail.com>
2025-08-14 19:40:12 +0100

    Fixed the alignment of the carousel for both desktop and mobile.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
     slidesPerView: 'auto',
     spaceBetween: 24,
     breakpoints: {
-      0:    { slidesPerView: 1, spaceBetween: 12,  centeredSlides: false, watchOverflow: true },
-      768: { slidesPerView: 1, spaceBetween: 28 },
-      1024:{ slidesPerView: 3, spaceBetween: 40 }
+      0:    { slidesPerView: 'auto', spaceBetween: 12,  centeredSlides: true, watchOverflow: true },
+      768: { slidesPerView: 1, spaceBetween: 28,  },
+      1024:{ slidesPerView: 2, spaceBetween: 40, centeredSlides: true, watchOverflow: true }
     },
     watchOverflow: false
   });
